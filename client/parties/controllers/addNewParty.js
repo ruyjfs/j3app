ec8762d3e0f27cea4b3661afbc2afbf4f6f60a38
ruyjfs <ruyjfs@gmail.com>
2015-10-07 18:46:18 -0300

    [inep]

---
+++ client/parties/controllers/addNewParty.js
@@ -0,0 +1,16 @@
+angular.module("socially").controller("AddNewPartyCtrl", ['$scope', '$meteor', '$rootScope', '$state', '$mdDialog', 'parties',
+    function ($scope, $meteor, $rootScope, $state, $mdDialog, parties) {
+        $scope.newParty = {};
+        $scope.addNewParty = function () {
+            if($scope.newParty.name){
+                $scope.newParty.owner = $rootScope.currentUser._id;
+                parties.push($scope.newParty);
+                $scope.newParty = '';
+                $mdDialog.hide();
+            }
+        }
+        $scope.close = function () {
+            $mdDialog.hide();
+        }
+    }
+]);
--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,6 +1,19 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
     function($scope, $stateParams, $meteor){
-        $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+        $scope.party = $meteor.object(Parties, $stateParams.partyId);
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+        $scope.$meteorSubscribe('parties');
+
+        $scope.invite = function(user){
+            $meteor.call('invite', $scope.party._id, user._id).then(
+                function(data){
+                    console.log('success inviting', data);
+                },
+                function(err){
+                    console.log('failed', err);
+                }
+            );
+        };
 
         $scope.save = function() {
             $scope.party.save().then(function(numberOfDocs){
@@ -14,6 +27,48 @@ angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$statePara
         $scope.reset = function() {
             console.log($scope.party.reset());
             $scope.party.reset();
+        };
+
+        $scope.canInvite = function (){
+            if (!$scope.party)
+                return false;
+
+            return !$scope.party.public &&
+                $scope.party.owner === Meteor.userId();
+        };
+
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8,
+            events: {
+                click: function (mapModel, eventName, originalEventArgs) {
+                    if (!$scope.party)
+                        return;
+
+                    if (!$scope.party.location)
+                        $scope.party.location = {};
+
+                    $scope.party.location.latitude = originalEventArgs[0].latLng.lat();
+                    $scope.party.location.longitude = originalEventArgs[0].latLng.lng();
+                    //scope apply required because this event handler is outside of the angular domain
+                    $scope.$apply();
+                }
+            },
+            marker: {
+                options: { draggable: true },
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        if (!$scope.party.location)
+                            $scope.party.location = {};
+
+                        $scope.party.location.latitude = marker.getPosition().lat();
+                        $scope.party.location.longitude = marker.getPosition().lng();
+                    }
+                }
+            }
         };
     }
 ]);
