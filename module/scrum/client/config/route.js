0ab4a70caf717df61f1744853f261c3dd3b5b1c2
ruyjfs <ruyjfs@gmail.com>
2016-03-31 01:38:06 -0300

    Casa

--- module/scrum/client/config/route.js
+++ module/scrum/client/config/route.js
@@ -1,18 +1,20 @@
 angular.module("scrum").run(['$rootScope', '$state', function($rootScope, $state) {
-    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-        // We can catch the error thrown when the $requireUser promise is rejected
-        // and redirect the user back to the main page
-        if (error === 'AUTH_REQUIRED') {
-
-            $state.go('scrum');
-            console.log(toState);
-        }
-    });
+//    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+//        // We can catch the error thrown when the $requireUser promise is rejected
+//        // and redirect the user back to the main page
+//        if (error === 'AUTH_REQUIRED') {
+//
+//            $state.go('scrum');
+//            console.log(toState);
+//        }
+//    });
+//
+    console.log(Meteor.userId());
 
     if (!Meteor.userId() && $state.current.url !== '/scrum') {
         $state.go('scrum');
     }
-
+//
 }]);
 
 angular.module('scrum').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
